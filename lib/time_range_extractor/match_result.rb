813ea4cdfaa58bb5617ed787b2941e2b328c6a5a
Jeremy Baker <jhubert@gmail.com>
2019-11-20 13:18:06 -0600

    Wrap the parser and match result in the module

--- lib/time_range_extractor/match_result.rb
+++ lib/time_range_extractor/match_result.rb
@@ -1,57 +1,59 @@
 # frozen_string_literal: true
 
-class MatchResult
-  def initialize(match_data)
-    @match_data = match_data
-  end
-
-  def valid?
-    @match_data && (start_time.present? && end_time.present?)
-  end
-
-  def start_time
-    match_data[:start_time]
-  end
-
-  def start_period
-    @start_period ||= match_data[:start_period].presence || begin
-      force_start_period_to_am? ? 'am' : end_period
+module TimeRangeExtractor
+  class MatchResult
+    def initialize(match_data)
+      @match_data = match_data
     end
+
+    def valid?
+      @match_data && (start_time.present? && end_time.present?)
+    end
+
+    def start_time
+      match_data[:start_time]
+    end
+
+    def start_period
+      @start_period ||= match_data[:start_period].presence || begin
+        force_start_period_to_am? ? 'am' : end_period
+      end
+    end
+
+    def end_time
+      match_data[:end_time]
+    end
+
+    def end_period
+      match_data[:end_period]
+    end
+
+    def time_zone
+      match_data[:time_zone]
+    end
+
+    def start_time_string
+      [start_time, start_period, time_zone].compact.join(' ')
+    end
+
+    def end_time_string
+      [end_time, end_period, time_zone].compact.join(' ')
+    end
+
+    private
+
+    def range?
+      start_time.present? && end_time.present?
+    end
+
+    def force_start_period_to_am?
+      start_t = start_time.to_i
+      end_t = end_time.to_i
+
+      end_period.casecmp('pm') == 0 &&
+        (start_t > end_t || (end_t == 12 && start_t < end_t))
+    end
+
+    attr_reader :match_data
   end
-
-  def end_time
-    match_data[:end_time]
-  end
-
-  def end_period
-    match_data[:end_period]
-  end
-
-  def time_zone
-    match_data[:time_zone]
-  end
-
-  def start_time_string
-    [start_time, start_period, time_zone].compact.join(' ')
-  end
-
-  def end_time_string
-    [end_time, end_period, time_zone].compact.join(' ')
-  end
-
-  private
-
-  def range?
-    start_time.present? && end_time.present?
-  end
-
-  def force_start_period_to_am?
-    start_t = start_time.to_i
-    end_t = end_time.to_i
-
-    end_period.casecmp('pm') == 0 &&
-      (start_t > end_t || (end_t == 12 && start_t < end_t))
-  end
-
-  attr_reader :match_data
 end
--- lib/time_range_extractor/parser.rb
+++ lib/time_range_extractor/parser.rb
@@ -2,59 +2,61 @@
 
 # Parse out time values from a string of text. Uses the provided date as the
 # basis for the DateTime generation.
-class Parser
-  PATTERN = /
-    (\A|\s|\() # space or round bracket, to support: "Call Jim (8-9pm)"
-    (
-      (?<start_time>(2[0-4]|1[0-9]|[1-9]):?([0-5][0-9])?)\s?
-      (?<start_period>am|pm)?\s?
-      (to|-|until|\s)\s?
-    )
-    (?<end_time>(2[0-4]|1[0-9]|[1-9]):?([0-5][0-9])?)?\s?
-    (?<end_period>am|pm)\s?
-    (?<time_zone>(
-      [ABCDEFGHIJKLMNOPRSTUVWY]
-      [A-Z]
-      [ACDEGHKLMNORSTUVW]?
-      [CDNSTW]?
-      [T]?
-    ))?\b
-  /xi.freeze
+module TimeRangeExtractor
+  class Parser
+    PATTERN = /
+      (\A|\s|\() # space or round bracket, to support: "Call Jim (8-9pm)"
+      (
+        (?<start_time>(2[0-4]|1[0-9]|[1-9]):?([0-5][0-9])?)\s?
+        (?<start_period>am|pm)?\s?
+        (to|-|until|\s)\s?
+      )
+      (?<end_time>(2[0-4]|1[0-9]|[1-9]):?([0-5][0-9])?)?\s?
+      (?<end_period>am|pm)\s?
+      (?<time_zone>(
+        [ABCDEFGHIJKLMNOPRSTUVWY]
+        [A-Z]
+        [ACDEGHKLMNORSTUVW]?
+        [CDNSTW]?
+        [T]?
+      ))?\b
+    /xi.freeze
 
-  def initialize(text, date: Date.current)
-    @text = text
-    @date = date
-  end
-
-  def call
-    match = PATTERN.match(@text)
-    result = MatchResult.new(match)
-    return nil unless result.valid?
-
-    time_range_from(result)
-  end
-
-  private
-
-  def time_range_from(match_result)
-    start_time = time_from_string(match_result.start_time_string)
-    end_time = time_from_string(match_result.end_time_string)
-
-    if start_time <= end_time
-      start_time..end_time
-    elsif start_time > end_time
-      start_time..(end_time + 1.day)
+    def initialize(text, date: Date.current)
+      @text = text
+      @date = date
     end
-  rescue ArgumentError
-    nil
-  end
 
-  def time_from_string(string)
-    time_parser.parse(string, @date.to_time)
-  end
+    def call
+      match = PATTERN.match(@text)
+      result = MatchResult.new(match)
+      return nil unless result.valid?
 
-  # :reek:UtilityFunction so that we can optionally include ActiveSupport
-  def time_parser
-    ::Time.zone || ::Time
+      time_range_from(result)
+    end
+
+    private
+
+    def time_range_from(match_result)
+      start_time = time_from_string(match_result.start_time_string)
+      end_time = time_from_string(match_result.end_time_string)
+
+      if start_time <= end_time
+        start_time..end_time
+      elsif start_time > end_time
+        start_time..(end_time + 1.day)
+      end
+    rescue ArgumentError
+      nil
+    end
+
+    def time_from_string(string)
+      time_parser.parse(string, @date.to_time)
+    end
+
+    # :reek:UtilityFunction so that we can optionally include ActiveSupport
+    def time_parser
+      ::Time.zone || ::Time
+    end
   end
 end
